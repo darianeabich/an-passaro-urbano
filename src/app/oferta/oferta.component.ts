7230c7d340646d8b5c36ca33e3c5b4ec0b7dd4fe
Dariane Abich <darianeabich@gmail.com>
2021-06-25 18:00:31 -0300

    Recuperando parâmetros da rota com Subscribe

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -11,8 +11,12 @@ export class OfertaComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    console.log("ID recuperado da rota: ", this.route.snapshot.params['id']);
+    // Snapshot
+    // console.log("ID recuperado da rota: ", this.route.snapshot.params['id']);
     // caso quisesse passar mais parametros na rota, só fazer o mesmo processo
+    this.route.params.subscribe((parametro: any) => {
+      console.log(parametro.id);
+    })
   }
 
 }
