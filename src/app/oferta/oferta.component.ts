05746f865f62ed9b118c9154b11af64f7cf5a468
Dariane Abich <darianeabich@gmail.com>
2021-06-28 17:12:41 -0300

    Complementando o observable: interval, stream com error() e/ou complete()

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, Observer } from 'rxjs';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-oferta',
@@ -24,10 +26,32 @@ export class OfertaComponent implements OnInit {
     this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
       .then((oferta: Oferta) => {
         this.oferta = oferta;
-      })
-    // this.route.params.subscribe((parametro: any) => {
-    //   console.log(parametro.id);
-    // })
+      });
+
+    // this.route.params.subscribe(
+    //   (parametro: any) => { console.log(parametro)},
+    //   (erro: any) => console.log(erro),
+    //   () => console.log('processamento foi classificado como concluído')
+    // );
+
+    // let tempo = interval(500);
+    // tempo.subscribe((resposta:number) => console.log(resposta));
+
+    //observable (observável)
+    let meuObservableTeste = new Observable((observer: Observer<number>) => {
+      observer.next(1);
+      observer.next(3);
+      //observer.error('ocorreu um erro que fechou a aplicação');
+      observer.complete();
+      observer.next(3);
+    });
+
+    //observable (observador)
+    meuObservableTeste.subscribe(
+      (resultado: number) => console.log(resultado + 10),
+      (erro: string) => console.log(erro),
+      () => console.log('Stream de eventos foi finalizada')
+    );
   }
 
 }
