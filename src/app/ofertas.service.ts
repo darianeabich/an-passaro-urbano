ce0e62e0728d15907cd52e178bdad508aafb46e4
Dariane Abich <darianeabich@gmail.com>
2021-06-28 14:46:30 -0300

    Adicionando mais dados no arquivo de banco de dados e recuperando dados 'Como usar' via HTTP Request

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -14,23 +14,31 @@ export class OfertasService {
   public getOfertas(): Promise<Oferta[]> {
     // efetuar uma requisição http
     // retornar uma promise Oferta[]
-    return this.http.get(`${URL_API}?destaque=true`)
+    return this.http.get(`${URL_API}/ofertas?destaque=true`)
       .toPromise()
       .then((resposta: any) => resposta);
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-    return this.http.get(`${URL_API}?categoria=${categoria}`)
+    return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta);
   }
 
   public getOfertaPorId(id: number): Promise<Oferta> {
-    return this.http.get(`${URL_API}?id=${id}`)
+    return this.http.get(`${URL_API}/ofertas?id=${id}`)
       .toPromise()
       .then((resposta: any) => {
         return resposta[0];
       });
+  }
+
+  public getComoUsuarOfertaPorId(id: number): Promise<string> {
+    return this.http.get(`${URL_API}/como-usar?id=${id}`)
+      .toPromise()
+      .then((resposta: any) => {
+        return resposta[0].descricao;
+      })
   }
 
   // public getOfertas2(): Promise<Oferta[]> {
