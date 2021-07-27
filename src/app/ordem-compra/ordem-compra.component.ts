74386424fccf68320a18eb754365c6b4d9d12a3c
Dariane Abich <darianeabich@gmail.com>
2021-07-27 18:03:15 -0300

    Fazendo validações com data binding para testar

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -12,6 +12,18 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = '';
   public formaPagamento: string = '';
 
+  //controles de validação dos campos com variáveis
+  public enderecoValido!: boolean;
+  public numeroValido!: boolean;
+  public complementoValido!: boolean;
+  public formaPagamentoValido!: boolean;
+
+  //controles de estado primitivo dos campos (pristine)
+  public enderecoEstadoPrimitivo: boolean = true;
+  public numeroEstadoPrimitivo: boolean = true;
+  public complementoPrimitivo: boolean = true;
+  public formaPagamentoEstadoPrimitivo: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -19,22 +31,52 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco;
-    console.log('endereco: ', this.endereco);
+    // console.log('endereco: ', this.endereco);
+
+    this.enderecoEstadoPrimitivo = false;
+    //se a string for maior que 3
+    if (this.endereco.length > 3) {
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
   }
 
   public atualizaNumero(numero: string): void {
     this.numero = numero;
-    console.log('numero: ', this.numero);
+    // console.log('numero: ', this.numero);
+
+    this.numeroEstadoPrimitivo = false;
+
+    if (this.numero.length >= 1) {
+      this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
+    }
   }
 
   public atualizaComplemento(complemento: string): void {
     this.complemento = complemento;
-    console.log('complemento: ', this.complemento);
+    // console.log('complemento: ', this.complemento);
+
+    this.complementoPrimitivo = false;
+
+    if (this.complemento.length >= 1) {
+      this.complementoValido = true;
+    }
   }
 
   public atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento;
-    console.log('formaPagamento: ', this.formaPagamento);
+    // console.log('formaPagamento: ', this.formaPagamento);
+
+    this.formaPagamentoEstadoPrimitivo = false;
+
+    if (this.formaPagamento.length > 0) {
+      this.formaPagamentoValido = true;
+    } else {
+      this.formaPagamentoValido = false;
+    }
   }
 
 }
